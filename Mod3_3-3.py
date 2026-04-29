0ac430a9d6396e67295a6ed3757a6cb47347a8cc
nunoalves79 <sdnunoalves@gmail.com>
2026-04-29 23:49:40 +0100

    ficheiro python

---
+++ Mod3_3-3.py
@@ -0,0 +1,10 @@
+numero=int(input('Introduz um número: '))
+if numero > 1:
+    for i in range(2, numero)
+        if numero % i == 0:
+            print('O número introduzido não é primo')
+            break
+    else:   
+        print('O número introduzido não é primo')
+else:
+    print('o numer não é primo')

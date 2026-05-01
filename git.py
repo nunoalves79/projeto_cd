ddfa20e70c040f7ad2cda5b8d6ec3ad7cbe09ee7
nunoalves79 <sdnunoalves@gmail.com>
2026-05-01 15:01:10 +0100

    o primeiro commit
    Condição para identificar valores que não são iguais

--- git.py
+++ git.py
@@ -2,3 +2,5 @@ num1 = int(input('Introduza um número: '))
 num2 = int(input('Introduza outro número: '))
 if num1 == num2:
     print('Os números são iguais')
+else: 
+    print('Os números são diferentes')

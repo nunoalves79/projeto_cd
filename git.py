1f8cc1ab3a03caa37351b2d6c01d9bf6b48c188b
nunoalves79 <sdnunoalves@gmail.com>
2026-05-01 21:52:22 +0100

    Organizar informação
    Organização: Estruturação de pastas e renomeação de ficheiros para clareza

--- git.py
+++
@@ -1,8 +0,0 @@
-#Testar a edição na branch NovaArea
-num1 = int(input('Introduza um número: '))
-num2 = int(input('Introduza outro número: '))
-if num1 == num2:
-    print('Os números são iguais')
-else: 
-    print('Os números são diferentes')
-#Testar um novo commit
---
+++ src/Verificador_numeros.py
@@ -0,0 +1,8 @@
+#Testar a edição na branch NovaArea
+num1 = int(input('Introduza um número: '))
+num2 = int(input('Introduza outro número: '))
+if num1 == num2:
+    print('Os números são iguais')
+else: 
+    print('Os números são diferentes')
+#Testar um novo commit
